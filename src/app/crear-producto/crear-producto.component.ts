456dd006ef6554c0411ef281e5f1c51f041cdfd5
franco <francomiret11@gmail.com>
2023-06-23 00:02:38 -0300

    feat: add excel to json converter poc

--- src/app/crear-producto/crear-producto.component.ts
+++ src/app/crear-producto/crear-producto.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import { read, utils } from 'xlsx';
 @Component({
   selector: 'app-crear-producto',
   templateUrl: './crear-producto.component.html',
-  styleUrls: ['./crear-producto.component.css']
+  styleUrls: ['./crear-producto.component.css'],
 })
 export class CrearProductoComponent {
+  public convertedJSON!: string;
+  $events: any;
 
+  public fileUpload(event: any) {
+    console.log(event)
+    const selectedFiles = event.target.files[0];
+    const fileReader = new FileReader();
+    fileReader.readAsBinaryString(selectedFiles);
+    fileReader.onload = (event: any) => {
+      let binaryData = event.target.result;
+      let workbook = read(binaryData, { type: 'binary' });
+      workbook.SheetNames.forEach((sheet) => {
+        const data = utils.sheet_to_json(workbook.Sheets[sheet]);
+        this.convertedJSON = JSON.stringify(data, undefined, 4);
+      });
+    };
+  }
 }
