535d618c0e0822aed3d6b9864e4f5e015fb4dc3a
franco <francomiret11@gmail.com>
2023-07-01 16:42:20 -0300

    refactor: agregar producto a un pedido

---
+++ src/app/crear-pedido/agregar-producto-dialog.ts
@@ -0,0 +1,130 @@
+import { Component, Inject } from '@angular/core';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+  MatDialogModule,
+} from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { Observable, map, startWith } from 'rxjs';
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  template: `
+    <h1 mat-dialog-title>Agregar producto {{ data.name }}</h1>
+    <div mat-dialog-content [formGroup]="form" style=" padding:5px;">
+      <mat-form-field appearance="outline" style="width: 100%;">
+        <mat-label>Producto</mat-label>
+        <input
+          type="text"
+          matInput
+          formControlName="nombre"
+          [matAutocomplete]="auto"
+        />
+        <mat-autocomplete autoActiveFirstOption #auto="matAutocomplete">
+          <mat-option
+            *ngFor="let option of filteredOptions | async"
+            [value]="option"
+          >
+            {{ option }}
+          </mat-option>
+        </mat-autocomplete>
+      </mat-form-field>
+      <mat-form-field appearance="outline" style="width: 100%;">
+        <mat-label>Cantidad</mat-label>
+        <input matInput type="number" formControlName="cantidad" />
+      </mat-form-field>
+    </div>
+    <div mat-dialog-actions>
+      <button mat-button (click)="onNoClick()">CERRAR</button>
+      <button
+        mat-raised-button
+        color="primary"
+        cdkFocusInitial
+        (click)="agregarProducto()"
+        [disabled]="form.invalid"
+      >
+        AGREGAR
+      </button>
+    </div>
+  `,
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    CommonModule,
+    MatAutocompleteModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+  ],
+})
+export class CrearProductoDialog {
+  public form: FormGroup = new FormGroup({
+    nombre: new FormControl('', Validators.required),
+    cantidad: new FormControl(1, Validators.required),
+    precioUnitario: new FormControl(1),
+    precioTotal: new FormControl(1),
+  });
+
+  public productos: any[] = [];
+  public filteredOptions?: Observable<string[]>;
+  public productosDelPedido: any[] = [];
+
+  constructor(
+    public dialogRef: MatDialogRef<CrearProductoDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {
+    const localStorageData = localStorage.getItem('marquitosData') ?? '';
+    this.productos = JSON.parse(localStorageData);
+
+    this.filteredOptions = this.form.controls['nombre'].valueChanges.pipe(
+      startWith(''),
+      map((value) => this._filter(value || ''))
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.productos
+      .map((x: any) => x.descripcion)
+      .filter((option) => option.toLowerCase().includes(filterValue));
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+  agregarProducto(): void {
+    console.log(this.form.value['nombre']);
+    const precioProducto = (
+      this.productos.find((x) => {
+        console.log(x);
+        return x.descripcion === this.form.value['nombre'];
+      }) as any
+    )?.final;
+    this.form.controls['precioUnitario'].setValue(precioProducto);
+    this.form.controls['precioTotal'].setValue(
+      precioProducto * this.form.controls['cantidad'].value
+    );
+
+    this.dialogRef.close(this.form.value);
+  }
+}
--- src/app/crear-pedido/crear-pedido.component.ts
+++ src/app/crear-pedido/crear-pedido.component.ts
@@ -5,6 +5,7 @@ import {
   FormGroup,
   FormsModule,
   ReactiveFormsModule,
+  Validators,
 } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
@@ -14,6 +15,9 @@ import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { RouterModule } from '@angular/router';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { CrearProductoDialog } from './agregar-producto-dialog';
+import { CommonModule } from '@angular/common';
 
 export interface Producto {
   nombre: string;
@@ -22,12 +26,6 @@ export interface Producto {
   precioTotal: number;
 }
 
-const ELEMENT_DATA: Producto[] = [
-  { cantidad: 1, nombre: 'Hydrogen', precioUnitario: 1.0079, precioTotal: 3 },
-  { cantidad: 2, nombre: 'Helium', precioUnitario: 4.0026, precioTotal: 3 },
-  { cantidad: 3, nombre: 'Lithium', precioUnitario: 6.941, precioTotal: 3 },
-  { cantidad: 4, nombre: 'Beryllium', precioUnitario: 9.0122, precioTotal: 3 },
-];
 @Component({
   selector: 'app-crear-pedido',
   templateUrl: './crear-pedido.component.html',
@@ -44,17 +42,21 @@ const ELEMENT_DATA: Producto[] = [
     MatTableModule,
     MatIconModule,
     RouterModule,
+    MatDialogModule,
+    CommonModule,
   ],
 })
 export class CrearPedidoComponent {
-  public control = new FormControl('');
+  constructor(public dialog: MatDialog) {
+    localStorage.setItem('pedido', '');
+  }
   public form: FormGroup = new FormGroup({
-    cliente: new FormControl(''),
-    direccion: new FormControl(''),
-    productos: new FormArray([]),
+    cliente: new FormControl('', Validators.required),
+    direccion: new FormControl('', Validators.required),
+    productos: new FormControl([], Validators.required),
     alcaraciones: new FormControl(''),
     fechaDeCreacion: new FormControl(Date.now()),
-    estado: new FormControl('pendiente'),
+    estado: new FormControl('pendiente', Validators.required),
   });
   displayedColumns: string[] = [
     'nombre',
@@ -63,5 +65,23 @@ export class CrearPedidoComponent {
     'precioTotal',
     'actions',
   ];
-  dataSource = ELEMENT_DATA;
+  dataSource = this.form.controls['productos'].valueChanges;
+
+  public crearPedido() {
+    console.log();
+    localStorage.setItem('pedido', this.form.value);
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(CrearProductoDialog, {
+      data: {},
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      this.form.controls['productos'].setValue([
+        ...this.form.controls['productos'].value,
+        result,
+      ]);
+    });
+  }
 }
